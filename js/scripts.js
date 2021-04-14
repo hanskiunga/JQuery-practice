de421ce1ad19203037fb3fdaef1e007d8b881e96
hans <hakiunga@gmail.com>
2021-04-14 06:58:48 +0300

    add walrus image to folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
-/*$(document).ready(function(){
-    $('h1').click(function(){
-        alert('This is a header');
-        // alert('I told you, THIS IS A HEADER.');
+$(document).ready(function() {
+    $(".clickable").click(function() {
+      $("#walrus-showing").toggle();
+    //   $("#walrus-showing").fadeIn();
+      $("#walrus-hidden").toggle();
+    //   $("#walrus-showing").slideToggle();
     });
-    $('p').click(function(){
-        alert('This is a paragraph')
-    });
-
-    $('img').click(function(){
-        alert('This is an image')
-    });
-});*/
-$(document).ready(function(){
-    $("p").click(function)({
-        $("img").show();
-    });
-});
+  });
